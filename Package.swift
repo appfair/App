56ec81353fa9e82d8914b313767c4842357704ff
appfairz <appfairz@appfair.net>
2021-10-28 22:14:17 -0400

    release: 0.6.126

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "App",
     defaultLocalization: "en",
-    platforms: [ .macOS(.v11), .iOS(.v14) ],
+    platforms: [ .macOS(.v12), .iOS(.v15) ],
     products: [ .library(name: "App", targets: ["App"]) ],
     dependencies: [
         .package(name: "Fair", url: "https://fair-ground.org/Fair.git", .branch("main")),
