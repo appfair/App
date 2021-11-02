7bb383804aa885346d2af3782d7e1f18cf5f8d4e
appbopp <appbopp@tiqtiq.net>
2021-11-01 20:20:17 -0400

    Update AppContainer.swift

--- Sources/App/AppContainer.swift
+++ Sources/App/AppContainer.swift
@@ -49,7 +49,9 @@ public struct ContentView: View {
     public var body: some View {
         // A FairApp comes with built-in FairContentView behaviors.
         // The `.placeholder` content will display some info about your app.
-        FairContentView(.placeholder)
+        Text("Welcome to my App Fair app!")
+            .font(.largeTitle)
+            .foregroundColor(.secondary)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
