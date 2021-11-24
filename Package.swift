d9d9a410e076df92bcf9718336345fa39f9f5b7c
fairapps <fairapps@appfair.net>
2021-11-24 11:43:36 -0500

    release: 0.0.8

--- Package.swift
+++ Package.swift
@@ -10,9 +10,10 @@ let package = Package(
         // the Fair main branch must be the first dependency to pass integration
         .package(name: "Fair", url: "https://fair-ground.org/Fair.git", .branch("main")),
         // additional GitHub-hosted dependencies can be added below
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveCocoa.git", .upToNextMajor(from: "11.2.0")),
     ],
     targets: [
-        .target(name: "App", dependencies: [ .product(name: "FairApp", package: "Fair") ], resources: [.process("Resources"), .copy("Bundle")]),
+        .target(name: "App", dependencies: [ .product(name: "FairApp", package: "Fair"), "ReactiveCocoa" ], resources: [.process("Resources"), .copy("Bundle")]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
 )
