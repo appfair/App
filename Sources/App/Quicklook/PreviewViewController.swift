94f60d73524d60e1e183960d845443403c3ff14a
fairapps <fairapps@appfair.net>
2021-12-04 21:47:17 -0500

    release: 0.0.41

--- Sources/App/Quicklook/PreviewViewController.swift
+++ Sources/App/Quicklook/PreviewViewController.swift
@@ -1,10 +1,3 @@
-//
-//  PreviewViewController.swift
-//  Lottie Motion Quicklook
-//
-//  Created by Marc Prud'hommeaux on 12/4/21.
-//
-
 import FairApp
 
 #if canImport(Quartz)
@@ -15,7 +8,6 @@ import QuickLook
 #endif
 
 class PreviewViewController: UXViewController, QLPreviewingController {
-    
     override func loadView() {
         print("### LottieQuickLook: loading preview controller view")
         self.view = UXHostingView(rootView: Group {
@@ -23,28 +15,8 @@ class PreviewViewController: UXViewController, QLPreviewingController {
         })
     }
 
-    /*
-     * Implement this method and set QLSupportsSearchableItems to YES in the Info.plist of the extension if you support CoreSpotlight.
-     *
-    func preparePreviewOfSearchableItem(identifier: String, queryString: String?, completionHandler handler: @escaping (Error?) -> Void) {
-        // Perform any setup necessary in order to prepare the view.
-        
-        // Call the completion handler so Quick Look knows that the preview is fully loaded.
-        // Quick Look will display a loading spinner while the completion handler is not called.
-        handler(nil)
-    }
-     */
-    
     func preparePreviewOfFile(at url: URL, completionHandler handler: @escaping (Error?) -> Void) {
         print("### LottieQuickLook: preparePreviewOfFile", url)
-
-        // Add the supported content types to the QLSupportedContentTypes array in the Info.plist of the extension.
-        
-        // Perform any setup necessary in order to prepare the view.
-        
-        // Call the completion handler so Quick Look knows that the preview is fully loaded.
-        // Quick Look will display a loading spinner while the completion handler is not called.
-        
         handler(nil)
     }
 }
