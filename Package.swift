fd0f196a4d08feffc02f100eba3d56f4b90f7d89
appfairz <appfairz@appfair.net>
2021-12-22 17:45:38 -0500

    chore: checkpoint

--- Package.swift
+++ Package.swift
@@ -5,14 +5,14 @@ let package = Package(
     name: "App",
     defaultLocalization: "en",
     platforms: [ .macOS(.v12), .iOS(.v15) ],
-    products: [ .library(name: "App", targets: ["App"]) ],
+    products: [ .library(name: "App", type: .dynamic, targets: ["App"]) ],
     dependencies: [
         // the Fair main branch must be the *first* dependency
         .package(name: "Fair", url: "https://fair-ground.org/Fair.git", .branch("main")),
         // additional GitHub-hosted dependencies can be added below
     ],
     targets: [
-        .target(name: "App", dependencies: [ .product(name: "FairApp", package: "Fair") ], resources: [.process("Resources"), .copy("Bundle")]),
+        .target(name: "App", dependencies: [ .product(name: "FairApp", package: "Fair"), "SwiftyJSON" ], resources: [.process("Resources"), .copy("Bundle")]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
 )
