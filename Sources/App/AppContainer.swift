c6aa75b874c4ce48a1430c184af69720cf61311e
fairapps <fairapps@appfair.net>
2021-12-31 16:14:50 -0500

    release: 0.0.6

--- Sources/App/AppContainer.swift
+++ Sources/App/AppContainer.swift
@@ -1,20 +1,27 @@
 import FairApp
 import Media
-
-let mediaBundle = Bundle.mediaBundle
+import AVKit
 
 /// The main content view for the app.
 public struct ContentView: View {
+    public var body: some View {
+        PlayerView()
+            .environmentObject(MediaManager.shared)
+    }
+}
+
+public struct PlayerView: View {
     @EnvironmentObject var store: Store
+    @EnvironmentObject var manager: MediaManager
 
     public var body: some View {
-        VStack {
-            Text("Welcome to **\(Bundle.main.bundleName!)**")
-                .font(.largeTitle)
-            Text("(it doesn't do anything _yet_)")
-                .font(.headline)
+        VideoPlayer(player: manager.player) {
+            //Text("MEDIA")
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onAppear {
+            manager.stream(url: Bundle.mediaBundle.url(forResource: "Bundle/Sita_Sings_the_Blues", withExtension: "mp4"))
+        }
     }
 }
 
@@ -46,3 +53,52 @@ public struct AppSettingsView : View {
         .padding()
     }
 }
+
+@available(macOS 12.0, iOS 15.0, *)
+@MainActor final class MediaManager: NSObject, ObservableObject, AVPlayerItemMetadataOutputPushDelegate {
+    static let shared = MediaManager()
+
+    @Published var itemTitle: String? = nil
+    @Published var playerItem: AVPlayerItem?
+
+    let player: AVPlayer = AVPlayer()
+
+    private override init() {
+        super.init()
+    }
+
+    func stream(url: URL?) {
+        guard let url = url else {
+            itemTitle = nil
+            return player.replaceCurrentItem(with: nil)
+        }
+
+        let asset = AVAsset(url: url)
+
+        let item = AVPlayerItem(asset: asset, automaticallyLoadedAssetKeys: [AVPartialAsyncProperty<AVAsset>.isPlayable])
+        self.playerItem = item
+
+        let metaOutput = AVPlayerItemMetadataOutput(identifiers: [
+            AVMetadataIdentifier.commonIdentifierTitle.rawValue,
+        ])
+        metaOutput.setDelegate(self, queue: DispatchQueue.main)
+        item.add(metaOutput)
+
+        player.replaceCurrentItem(with: item)
+    }
+
+    func outputSequenceWasFlushed(_ output: AVPlayerItemOutput) {
+        dbg(output)
+    }
+
+    func metadataOutput(_ output: AVPlayerItemMetadataOutput, didOutputTimedMetadataGroups groups: [AVTimedMetadataGroup], from track: AVPlayerItemTrack?) {
+
+        dbg("received metadata:", output, "groups:", groups, "track:", track)
+
+        if let group = groups.first,
+           let item = group.items.first {
+            self.itemTitle = item.stringValue ?? "Unknown"
+        }
+    }
+}
+
