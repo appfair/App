63551b2178fface685e6e019c4f4229a4d71fbd9
fairapps <fairapps@appfair.net>
2022-01-04 16:47:20 -0500

    release: 0.0.25

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     targets: [
         .target(name: "App", dependencies: [
             .product(name: "FairApp", package: "Fair"),
+            "SnapKit",
         ], resources: [.process("Resources"), .copy("Bundle")]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
