43ff618da5094e121c9244a96252f0605bc80080
appfairz <appfairz@appfair.net>
2022-02-05 09:25:46 -0500

    release: 0.7.191

--- fetchbrew.sh
+++ fetchbrew.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -e
+ZIPFILE="Sources/App/Bundle/brew.zip"
 echo "Fetching latest brew from https://github.com/App-Fair/brew (be sure to merge from upstream first)"
-curl -fSL -o Sources/App/Bundle/brew.zip https://github.com/App-Fair/brew/zipball/HEAD 
+curl -fSL -o ${ZIPFILE} https://github.com/App-Fair/brew/zipball/HEAD 
+# need to also delete the test fixtures because they contain un-signed binaries that will cause the notary service to fail
+ls -lah ${ZIPFILE}
+echo "Removing unneeded test support files"
+zip -d ${ZIPFILE} '*/Library/Homebrew/test/support/**'
+#zip -d ${ZIPFILE} '*/docs/**'
+ls -lah ${ZIPFILE}
