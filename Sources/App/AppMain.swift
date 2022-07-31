ede3f161060fbdb523c74336298be531a6a1eca9
appfairz <appfairz@appfair.net>
2022-07-30 21:23:26 -0400

    release: 0.8.90

--- Sources/App/AppMain.swift
+++ Sources/App/AppMain.swift
@@ -5,6 +5,7 @@ import FairApp
 ///
 /// - Note: This source in `AppMain.swift` must not be modified; 
 ///         App customization should be done in `AppContainer.swift`
+@available(macOS 12.0, iOS 15.0, *)
 @main public enum AppContainer : FairApp.FairContainer {
     public static func main() async throws { try await launch(bundle: Bundle.module) }
 }
