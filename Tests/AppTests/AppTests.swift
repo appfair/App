bc50820aa5a736c355683c0ad1c9fd156388f35a
appfairz <appfairz@appfair.net>
2022-08-26 14:04:45 -0400

    chore: checkpoint

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -9,8 +9,25 @@
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Affero General Public License for more details.
 
- You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ The full text of the GNU Affero General Public License can be
+ found in the COPYING.txt file or at https://www.gnu.org/licenses/
+
+ Linking this library statically or dynamically with other modules is
+ making a combined work based on this library.  Thus, the terms and
+ conditions of the GNU Affero General Public License cover the whole
+ combination.
+
+ As a special exception, the copyright holders of this library give you
+ permission to link this library with independent modules to produce an
+ executable, regardless of the license terms of these independent
+ modules, and to copy and distribute the resulting executable under
+ terms of your choice, provided that you also meet, for each linked
+ independent module, the terms and conditions of the license of that
+ module.  An independent module is a module which is not derived from
+ or based on this library.  If you modify this library, you may extend
+ this exception to your version of the library, but you are not
+ obligated to do so.  If you do not wish to do so, delete this
+ exception statement from your version.
  */
 import Swift
 import XCTest
