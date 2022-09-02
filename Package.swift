a0edd0970504d4aa4a9bf31bd2b0d8e8bcf57698
fairapps <fairapps@appfair.org>
2022-09-02 18:16:44 -0400

    release: 0.0.2

--- Package.swift
+++ Package.swift
@@ -8,10 +8,12 @@ let package = Package(
     products: [ .library(name: "App", type: .dynamic, targets: ["App"]) ],
     dependencies: [
         .package(url: "https://github.com/fair-ground/Fair", from: "0.5.0"), // required
+        .package(url: "https://github.com/tiqtiq/WeatherTiq", branch: "main"),
     ],
     targets: [
         .target(name: "App", dependencies: [
             .product(name: "FairApp", package: "Fair"), // required
+            .product(name: "WeatherTiq", package: "WeatherTiq"),
         ], resources: [
             .process("Resources"),
             .copy("Bundle"),
@@ -20,37 +22,3 @@ let package = Package(
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
 )
-
-// MARK: fair-ground package validation
-
-// The following validations are required in order for the package
-// to be accepted by the appfair's integration-release workflow.
-//
-// These lines can be removed from your project, but the
-// rules will be enforced during the `integrate` phase regardless.
-
-precondition(package.name == "App", "Package.swift name must be 'App', but was: '\(package.name)'")
-precondition(!package.dependencies.isEmpty, "Package.swift must have at least one dependency")
-precondition(package.dependencies.first?.url == "https://github.com/fair-ground/Fair", "first Package.swift dependency must be 'https://github.com/fair-ground/Fair', but was: '\(String(describing: package.dependencies.first?.url ?? ""))'")
-
-precondition(package.products.count == 1, "Package.swift must have exactly one product")
-precondition(package.products.first?.name == "App", "Package.swift product must be named 'App', but was: '\(package.products.first?.name ?? "")'")
-
-// validate target names and source paths
-
-precondition(package.targets.count == 2, "package must have exactly two targets named 'App' and 'AppTests'")
-
-precondition(package.targets.first?.name == "App", "first target must be named 'App', but was: '\(package.targets.first?.name ?? "")'")
-precondition(package.targets.first?.path == nil || package.targets.first?.path == "Sources", "first target path must be named 'Sources', but was: '\(package.targets.first?.path ?? "")'")
-precondition(package.targets.first?.sources == nil, "first target sources must be empty")
-
-precondition(package.targets.last?.name == "AppTests", "second target must be named 'AppTests', but was: \(package.targets.last?.name ?? "")")
-precondition(package.targets.last?.path == nil || package.targets.last?.path == "Tests", "second target must be named 'Tests', but was: '\(package.targets.last?.path ?? "")'")
-precondition(package.targets.last?.sources == nil, "second target sources must be empty")
-
-precondition(package.targets.first?.dependencies.isEmpty == false, "package target must have at least one dependency")
-
-// Target.Depencency is opaque and non-equatable, so resort to using the description for validation
-precondition(String(describing: package.targets.first!.dependencies.first!).hasPrefix("productItem(name: \"FairApp\", package: Optional(\"Fair\")") == true, "first package dependency must be FairApp")
-
-
--- Sources/App/AppContainer.swift
+++ Sources/App/AppContainer.swift
@@ -1,17 +1,91 @@
 import FairApp
+import WeatherTiq
+
+
+//@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+//let service = WeatherService.weatherKit
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+let service = WeatherService.shared
 
 /// The main content view for the app.
 public struct ContentView: View {
     @EnvironmentObject var store: Store
+    @State var coords = Coords(latitude: 42.35843, longitude: -71.05977, altitude: 0) // Boston
 
     public var body: some View {
         VStack {
-            Text("Welcome to **\(Bundle.main.bundleName!)**")
-                .font(.largeTitle)
-            Text("(it doesn't do anything _yet_)")
+            Text(Bundle.main.bundleName!)
                 .font(.headline)
+            Spacer()
+            Form {
+                Section {
+                    TextField("Latitude:", value: $coords.latitude, format: .number, prompt: Text("Latitude"))
+                    TextField("Longitude:", value: $coords.longitude, format: .number, prompt: Text("Longitude"))
+                    TextField("Altitude:", value: $coords.altitude, format: .number, prompt: Text("Altitude"))
+                } header: {
+                    WeatherView(coords: coords)
+                        .font(.title)
+                }
+
+            }
+            .keyboardType(.decimalPad)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct Coords : Hashable, Identifiable {
+    var latitude: Double
+    var longitude: Double
+    var altitude: Double
+
+    var id: Self { self }
+}
+
+public struct WeatherView: View {
+    let coords: Coords
+    @State private var weather: Weather? = nil
+    @State private var error: Error? = nil
+
+    public var body: some View {
+        VStack {
+            if let error = error {
+                HStack {
+                    Text("Error:")
+                    Text(error.localizedDescription)
+                }
+            } else if let weather = weather {
+                HStack {
+                    Text("Temperature:")
+                    Text(weather.currentWeather.temperature.description)
+                }
+                HStack {
+                    Text("Wind:")
+                    Text(weather.currentWeather.wind.speed.description)
+                    Text(weather.currentWeather.wind.compassDirection.description)
+                }
+
+            } else {
+                Text("Loading…")
+            }
+        }
+        .task(id: coords, priority: .userInitiated) {
+            await fetchWeather(for: coords)
+        }
+    }
+
+    private func fetchWeather(for coords: Coords) async {
+        self.weather = nil
+        self.error = nil
+
+        do {
+            self.weather = try await service.weather(for: .init(latitude: coords.latitude, longitude: coords.longitude, altitude: coords.altitude))
+            dbg("fetched weather:", weather)
+        } catch {
+            dbg("error fetching weather:", error)
+            self.error = error
+        }
     }
 }
 
