a52b94bffd33532120e01d31572eb58a4f64da84
fairapps <fairapps@appfair.org>
2022-09-05 02:04:46 -0400

    release: 0.0.8

--- Sources/App/AppContainer.swift
+++ Sources/App/AppContainer.swift
@@ -6,30 +6,37 @@ import JXKit
 //@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
 //let service = WeatherService.weatherKit
 
-@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
-let service = WeatherService.shared
-
-// The shared script context
-let ctx = JXKit.JXContext()
-
 /// The main content view for the app.
 public struct ContentView: View {
     @EnvironmentObject var store: Store
-    @State var coords = Coords(latitude: 42.35843, longitude: -71.05977, altitude: 0) // Boston
+    @State var coords = Store.defaultCoords
 
     public var body: some View {
+        WeatherFormView(coords: $coords)
+            .padding()
+    }
+}
+
+struct WeatherFormView : View {
+    @Binding var coords: Coords
+
+    var body: some View {
         VStack {
             Text(Bundle.main.bundleName!)
                 .font(.headline)
             Spacer()
+            GroupBox("Current Weather") {
+                WeatherView(coords: coords)
+            }
+            .font(.title)
+            .padding()
+            Spacer()
+
             Form {
                 Section {
                     TextField("Latitude:", value: $coords.latitude, format: .number, prompt: Text("Latitude"))
                     TextField("Longitude:", value: $coords.longitude, format: .number, prompt: Text("Longitude"))
                     TextField("Altitude:", value: $coords.altitude, format: .number, prompt: Text("Altitude"))
-                } header: {
-                    WeatherView(coords: coords)
-                        .font(.title)
                 }
 
             }
@@ -41,64 +48,68 @@ public struct ContentView: View {
     }
 }
 
-struct Coords : Hashable, Identifiable {
+public struct Coords : Hashable, Codable {
     var latitude: Double
     var longitude: Double
     var altitude: Double
 
-    var id: Self { self }
+    public init(latitude: Double, longitude: Double, altitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+    }
+
 }
 
-public struct WeatherView: View {
+extension Coords : Identifiable {
+    public var id: Self { self }
+}
+
+public struct WeatherView: View, Equatable {
     let coords: Coords
-    @State private var weather: Weather? = nil
-    @State private var error: Error? = nil
+
+    public var body: some View {
+        WeatherFetcherView(coords: coords)
+    }
+}
+
+private struct WeatherFetcherView: View {
+    let coords: Coords
+    @State private var weatherResult: Result<Weather, Error>? = .none
 
     public var body: some View {
         VStack {
-            if let error = error {
-                HStack {
-                    Text("Error:")
-                    Text(error.localizedDescription)
+            switch self.weatherResult {
+            case .none:
+                Text("Loading…")
+            case .failure(let error):
+                if error is CancellationError { // FIXME: this is only throws from Task.checkCancellation(), not from the URLSession task being cancelled
+                    // expected; happens when the user cancels the fetch
+                    Rectangle().fill(Color.cyan)
+                } else {
+                    HStack {
+                        Text("Error:")
+                        Text(error.localizedDescription)
+                    }
                 }
-            } else if let weather = weather {
+            case .success(let weather):
                 HStack {
                     Text("Temperature:")
-                    Text(weather.currentWeather.temperature.description)
+                    Text(weather.currentWeather.temperature, format: .measurement(width: .wide))
                 }
                 HStack {
                     Text("Wind:")
-                    Text(weather.currentWeather.wind.speed.description)
-                    Text(weather.currentWeather.wind.compassDirection.description)
+                    Text(weather.currentWeather.wind.speed, format: .measurement(width: .narrow))
+                    let dir = Text(weather.currentWeather.wind.direction, format: .measurement(width: .abbreviated))
+                    Text("\(weather.currentWeather.wind.compassDirection.description) (\(dir))")
                 }
-
-            } else {
-                Text("Loading…")
             }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .task(id: coords, priority: .userInitiated) {
-            await fetchWeather(for: coords)
-        }
-        .task {
-            do {
-                let result = try ctx.eval(script: "1+1")
-                dbg("JS result:", result)
-            } catch {
-                dbg("error running script: \(error)")
+            self.weatherResult = await Result {
+                try await Store.service.weather(for: .init(latitude: coords.latitude, longitude: coords.longitude, altitude: coords.altitude))
             }
-        }
-    }
-
-    private func fetchWeather(for coords: Coords) async {
-        self.weather = nil
-        self.error = nil
-
-        do {
-            self.weather = try await service.weather(for: .init(latitude: coords.latitude, longitude: coords.longitude, altitude: coords.altitude))
-            dbg("fetched weather:", weather)
-        } catch {
-            dbg("error fetching weather:", error)
-            self.error = error
         }
     }
 }
@@ -114,8 +125,28 @@ open class Store: SceneManager {
     /// Mutable persistent global state for the app using ``SwiftUI/AppStorage``.
     @AppStorage("someToggle") public var someToggle = false
 
+
+    @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+    public static let service = WeatherService(serviceURL: defaultWeatherServer)
+
+    // The shared script context for executing adjuncts
+    public static let ctx = JXKit.JXContext()
+
     public required init() {
     }
+
+    static var defaultWeatherServer: URL = {
+        Store.config["weather"]?["server"]?.str.flatMap(URL.init(string:)) ?? WeatherService.shared.serviceURL
+    }()
+
+    static var defaultCoords: Coords = {
+        let defloc = Store.config["weather"]?["default_location"] ?? [:]
+        let lat = defloc["latitude"]?.num
+        let lon = defloc["longitude"]?.num
+        let alt = defloc["altitude"]?.num
+        return Coords(latitude: lat ?? 42.35843, longitude: lon ?? -71.05977, altitude: alt ?? 0)
+    }()
+
 }
 
 public extension AppContainer {
