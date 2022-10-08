9e043bcc364f1e70dd013430af6df90a3ce1b7cd
fairapps <fairapps@appfair.org>
2022-10-08 19:32:46 -0400

    release: 0.9.328

--- project.swift
+++ project.swift
@@ -1,8 +1,11 @@
 import App
 import Foundation
 
-/// This is the entry point for the app, which is defined in ``App.AppContainer``.
-/// It is the only code that may be included in the Xcode `project.xcodeproj`,
+///
+/// **DO NOT MODIFY**
+///
+/// This is the entry point for the app, which is defined in App.AppContainer.
+/// It is the **only** code that may be included in the Xcode `project.xcodeproj`,
 /// and it must remain unmodified on order to the app to be eligable for App Fair distribution.
 @available(macOS 12.0, iOS 15.0, *)
 @main public enum App {
