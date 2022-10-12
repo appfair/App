8fc2e73e66a449f96a8cec476f816f506d81cafc
fairapps <fairapps@appfair.org>
2022-10-12 15:50:53 -0400

    release: 0.9.330

--- Sources/App/WelcomeView.swift
+++ Sources/App/WelcomeView.swift
@@ -40,7 +40,7 @@ struct WelcomeView : View {
                             graphic: NSLocalizedString("card-02-graphic", bundle: .module, value: "camera.macro", comment: "app intro card #2 graphic symbol name"))
         case 3:
             return makeCard(3,
-                            title: NSLocalizedString("card-03-banner", bundle: .module, value: "International", comment: "app intro card #3 banner markdown"),
+                            title: NSLocalizedString("card-03-banner", bundle: .module, value: "Internationalize", comment: "app intro card #3 banner markdown"),
                             subtitle: NSLocalizedString("card-03-caption", bundle: .module, value: "App Fair Apps are International", comment: "app intro card #3 caption markdown"),
                             body: NSLocalizedString("card-03-content", bundle: .module, value: "Describe how to use the app by editing the localized `.strings` file and updating the `card-03-content` key.", comment: "app intro card #3 content markdown"),
                             graphic: NSLocalizedString("card-03-graphic", bundle: .module, value: "globe.europe.africa", comment: "app intro card #3 graphic symbol name"))
@@ -48,7 +48,7 @@ struct WelcomeView : View {
             return makeCard(4,
                             title: NSLocalizedString("card-04-banner", bundle: .module, value: "Enjoy", comment: "app intro card #4 banner markdown"),
                             subtitle: NSLocalizedString("card-04-caption", bundle: .module, value: "Have fun making your app!", comment: "app intro card #4 caption markdown"),
-                            body: NSLocalizedString("card-04-content", bundle: .module, value: "Make something fun and useful for yourself, your community, and your world.", comment: "app intro card #4 content markdown"),
+                            body: NSLocalizedString("card-04-content", bundle: .module, value: "Make something wonderful for yourself, your community, and your world.", comment: "app intro card #4 content markdown"),
                             graphic: NSLocalizedString("card-04-graphic", bundle: .module, value: "figure.wave", comment: "app intro card #4 graphic symbol name"))
         // more cards can be added here
         default:
@@ -56,12 +56,12 @@ struct WelcomeView : View {
         }
 
         func makeCard(_ index: Int, title: String, subtitle: String, body: String, graphic: String) -> SymbolCard? {
-            func localized(_ value: String) -> String? {
+            func checkLocalized(_ value: String) -> String? {
                 // only show cards that have a localization set
                 if value.hasPrefix("card-") { return nil }
                 return value
             }
-            guard let title = localized(title) else {
+            guard let title = checkLocalized(title) else {
                 return nil
             }
 
@@ -69,7 +69,7 @@ struct WelcomeView : View {
                 .accent, .orange, .teal, .brown, .cyan, .blue, .green, .red, .indigo, .purple, .mint, .yellow, .pink
             ]
             let color = colors[((index - 1) % colors.count)]
-            return Card(title: title, subtitle: localized(subtitle), body: localized(body), backgroundColors: [.init(color)], graphic: .init(stringLiteral: graphic))
+            return Card(title: title, subtitle: checkLocalized(subtitle), body: checkLocalized(body), backgroundColors: [.init(color)], graphic: .init(stringLiteral: graphic))
         }
     }
 }
